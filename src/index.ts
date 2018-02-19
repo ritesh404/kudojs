862625fbfdd59122f39cf8bdafd274f0404a0730
blitzritz <blitzritz@gmail.com>
2018-02-19 16:05:16 +0530

    Lint fixes for index

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
 /* istanbul ignore file */
 import {
-  id,
-  once,
-  fmap,
-  bimap,
-  chain,
-  caseOf,
-  curry,
-  ncurry,
-  compose,
-  liftAn,
-  liftA1,
-  liftA2,
-  liftA3,
-  liftA4,
-  liftA5,
-  eitherToMaybe,
-  maybeToEither
+    bimap,
+    caseOf,
+    chain,
+    compose,
+    curry,
+    eitherToMaybe,
+    fmap,
+    id,
+    liftA1,
+    liftA2,
+    liftA3,
+    liftA4,
+    liftA5,
+    liftAn,
+    maybeToEither,
+    ncurry,
+    once
 } from "./functions/helpers";
 
-//Algebraic Data Types
-import Pair from "./data-types/pair";
+// Algebraic Data Types
 import Either from "./data-types/either";
 import Maybe from "./data-types/maybe";
+import Pair from "./data-types/pair";
 import Task from "./data-types/task";
 
 export default {
-  id,
-  once,
-  fmap,
-  bimap,
-  chain,
-  caseOf,
-  curry,
-  ncurry,
-  compose,
-  liftAn,
-  liftA1,
-  liftA2,
-  liftA3,
-  liftA4,
-  liftA5,
-  eitherToMaybe,
-  maybeToEither,
-  Pair,
-  Either,
-  Maybe,
-  Task
+    Either,
+    Maybe,
+    Pair,
+    Task,
+    bimap,
+    caseOf,
+    chain,
+    compose,
+    curry,
+    eitherToMaybe,
+    fmap,
+    id,
+    liftA1,
+    liftA2,
+    liftA3,
+    liftA4,
+    liftA5,
+    liftAn,
+    maybeToEither,
+    ncurry,
+    once
 };
